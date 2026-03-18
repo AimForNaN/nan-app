82724b40fd959be4420cfd6f0fe6ce51e2e78bcd
AimForNaN <aimfornan@localhost>
2026-03-18 06:07:08 -0400

    Refactoring.

--- src/App.php
+++ src/App.php
@@ -25,35 +25,35 @@ use Psr\Http\Server\{
 
 class App implements \ArrayAccess, PsrContainerInterface, PsrRequestHandlerInterface {
 	public function __construct(
-		protected PsrContainerInterface $services = new Container(),
-		protected PsrRequestHandlerInterface $middleware = new Middleware(),
+		protected PsrContainerInterface $_services = new Container(),
+		protected PsrRequestHandlerInterface $_middleware = new Middleware(),
 	) {
 		if (
-			$this->services instanceof ContainerInterface &&
-			$this->middleware instanceof PsrContainerInterface
+			$this->_services instanceof ContainerInterface &&
+			$this->_middleware instanceof PsrContainerInterface
 		) {
-			$this->services->addDelegate($this->middleware);
+			$this->_services->addDelegates($this->_middleware);
 		}
 	}
 
 	public function get(string $id) {
-		return $this->services->get($id);
+		return $this->_services->get($id);
 	}
 
 	public function handle(PsrServerRequestInterface $request): PsrResponseInterface {
-		if ($this->middleware instanceof \Iterator) {
-			$this->middleware->rewind();
+		if ($this->_middleware instanceof \Iterator) {
+			$this->_middleware->rewind();
 		}
 
-		return $this->middleware->handle($request, $this);
+		return $this->_middleware->handle($request, $this);
 	}
 
 	public function has(string $id): bool {
-		return $this->services->has($id);
+		return $this->_services->has($id);
 	}
 
 	public function offsetExists(mixed $offset): bool {
-		return $this->services->has($offset);
+		return $this->_services->has($offset);
 	}
 
 	/**
@@ -61,20 +61,20 @@ class App implements \ArrayAccess, PsrContainerInterface, PsrRequestHandlerInter
 	 * @throws NotFoundExceptionInterface
 	 */
 	public function offsetGet(mixed $offset): mixed {
-		return $this->services->get($offset);
+		return $this->_services->get($offset);
 	}
 
 	public function offsetSet(mixed $offset, mixed $value): void {
-		if ($this->services instanceof \ArrayAccess) {
-			$this->services->offsetSet($offset, $value);
+		if ($this->_services instanceof \ArrayAccess) {
+			$this->_services->offsetSet($offset, $value);
 		} else {
 			\trigger_error('Could not register service!', E_USER_WARNING);
 		}
 	}
 
 	public function offsetUnset(mixed $offset): void {
-		if ($this->services instanceof \ArrayAccess) {
-			$this->services->offsetUnset($offset);
+		if ($this->_services instanceof \ArrayAccess) {
+			$this->_services->offsetUnset($offset);
 		}
 	}
 
@@ -89,8 +89,8 @@ class App implements \ArrayAccess, PsrContainerInterface, PsrRequestHandlerInter
 	}
 
 	public function use(PsrMiddlewareInterface $middleware): void {
-		if ($this->middleware instanceof \ArrayAccess) {
-			$this->middleware->offsetSet($middleware::class, $middleware);
+		if ($this->_middleware instanceof \ArrayAccess) {
+			$this->_middleware->offsetSet($middleware::class, $middleware);
 		} else {
 			\trigger_error('Could not register middleware!', E_USER_WARNING);
 		}
@@ -98,13 +98,13 @@ class App implements \ArrayAccess, PsrContainerInterface, PsrRequestHandlerInter
 
 	public function withMiddleware(PsrRequestHandlerInterface $middleware): App {
 		$new = clone $this;
-		$new->middleware = $middleware;
+		$new->_middleware = $middleware;
 		return $new;
 	}
 
 	public function withServices(PsrContainerInterface $container): App {
 		$new = clone $this;
-		$new->services = $container;
+		$new->_services = $container;
 		return $new;
 	}
 }
