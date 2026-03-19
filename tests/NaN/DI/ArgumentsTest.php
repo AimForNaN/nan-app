6c870860a4418804223715c69427e924fc3cf2e6
AimForNaN <aimfornan@localhost>
2026-03-19 02:18:32 -0400

    Refactoring.

--- tests/NaN/DI/ArgumentsTest.php
+++ tests/NaN/DI/ArgumentsTest.php
@@ -10,14 +10,20 @@ describe('Dependency Injection: Arguments', function () {
 		$container = new Container();
 		$callable = function (int $test1, string $test2) {};
 		$arguments = Arguments::fromCallable($callable, [1, '']);
-		expect($arguments)->toHaveCount(2);
-		expect($arguments->resolve($container))->toHaveCount(2);
+		expect($arguments)
+			->toHaveCount(2)
+			->and($arguments->resolve($container))
+				->toHaveCount(2)
+		;
 	});
 
 	test('Iteration', function () {
 		$callable = function (int $test1, string $test2) {};
 		$arguments = Arguments::fromCallable($callable);
-		expect($arguments)->toHaveCount(2);
-		expect([...$arguments])->toHaveCount(2);
+		expect($arguments)
+			->toHaveCount(2)
+			->and([...$arguments])
+				->toHaveCount(2)
+		;
 	});
 });
