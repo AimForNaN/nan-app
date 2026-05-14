ba497554c1c64f9c324082ebec9e2d11dda4ea6a
AimForNaN <aimfornan@localhost>
2026-05-14 18:33:43 -0400

    Update examples.

--- examples/config/services.php
+++ examples/config/services.php
@@ -1,4 +1,16 @@
 <?php
 
+use NaN\Http\{ResponseFactory, ServerRequestFactory};
+use Psr\Http\{
+	Message\ResponseFactoryInterface,
+	Message\ServerRequestInterface,
+};
+
 return new NaN\DI\Container([
+	ResponseFactoryInterface::class => ResponseFactory::class,
+	ServerRequestInterface::class => new ServerRequestFactory()->createServerRequest(
+		$_SERVER['REQUEST_METHOD'],
+		$_SERVER['REQUEST_URI'],
+		$_SERVER,
+	),
 ]);
